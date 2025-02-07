37dbcb1416f141d10a3cb669f5e0daad14066d0a
Paul Landers <444549+p3l6@users.noreply.github.com>
2025-02-07 19:26:19 +0000

    abcm2ps: add 8.15.0 bottle.

--- Formula/abcm2ps.rb
+++ Formula/abcm2ps.rb
@@ -5,6 +5,13 @@ class Abcm2ps < Formula
   sha256 "c6daa476cf0f8aa07ab6cd3630ba4e5be795dd46ccd21579046f162a82600702"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/p3l6/homebrew-tap/releases/download/abcm2ps-8.15.0"
+    sha256 arm64_sequoia: "e98fa439d440b7ae6310c2e4f760142c47c7e62c9100f3394ee3bbb1cf8f3c55"
+    sha256 ventura:       "0e9f685567a26213abc3c1d1ad17dbe43ff66b4c58ef453030bab6ff15dbfdb4"
+    sha256 x86_64_linux:  "c4850affc99184f5eb202430b5221372f3a222fb7e001a936b5c7dd0a346b1bc"
+  end
+
   depends_on "pkgconf" => :build
 
   def install

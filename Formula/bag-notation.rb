05ca709d0dd15c838c69372e78c5c645bb887c1b
Paul Landers <444549+p3l6@users.noreply.github.com>
2025-02-07 19:26:20 +0000

    bag-notation: add 0.2.0 bottle.

--- Formula/bag-notation.rb
+++ Formula/bag-notation.rb
@@ -5,6 +5,14 @@ class BagNotation < Formula
   sha256 "a91b05c7fb9c7d84d3f5c05f1877e3c432cda4c88fd1dc37fecfc94768148471"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/p3l6/homebrew-tap/releases/download/bag-notation-0.2.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d35311ff7e7fb0516c4d60bdd4847d9ae3d4a27fd0165f65fd84f1a5741f09d4"
+    sha256 cellar: :any_skip_relocation, ventura:       "f17d57feddb7972937a322f475cdd43bb33a0280e4557ee4da985c09af3a3fe7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05cfd40f796c04b3742d8004eda80ddc3686f0f54eec42c453eb8dad8fdc881d"
+  end
+
   depends_on "ghostscript" => :recommended
   depends_on "p3l6/tap/abcm2ps" => :recommended
 

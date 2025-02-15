5a9dc61212dd787c3af7407b7e07a6a69c144403
Paul Landers <paul@thelanders.family>
2025-02-14 20:34:50 -0500

    bag v0.4.0

--- Formula/bag-notation.rb
+++ Formula/bag-notation.rb
@@ -1,8 +1,8 @@
 class BagNotation < Formula
   desc "Compiler for .bag music files"
   homepage "https://github.com/p3l6/bag-notation"
-  url "https://github.com/p3l6/bag-notation/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "a91b05c7fb9c7d84d3f5c05f1877e3c432cda4c88fd1dc37fecfc94768148471"
+  url "https://github.com/p3l6/bag-notation/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "da74be69c05ee3416c3142d7df7ba33e6cb5320e5cb1037d13b8775d030eca63"
   license "MIT"
 
   bottle do

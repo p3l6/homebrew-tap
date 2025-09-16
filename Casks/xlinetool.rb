58303717919763c4dabb3e6da8f84dca3406e265
Paul Landers <paul@thelanders.family>
2025-09-16 13:54:48 -0400

    v1.1.0

--- Casks/xlinetool.rb
+++ Casks/xlinetool.rb
@@ -1,13 +1,13 @@
 cask "xlinetool" do
-  version "1.0.0"
-  sha256 "1bd32a2e2878306ee2678e89fc4b356492eec8f549fc77f832794d14e772a1d9"
+  version "1.1.0"
+  sha256 "2ae664c57e3f77446ee015eaa7a762486e34d5a5c285b57f7c058449e33922c9"
 
   url "https://github.com/p3l6/XLineTool/releases/download/v#{version}/XLineTool.zip"
   name "xlinetool"
   desc "Xcode extension providing duplicate line and insert newline after current"
   homepage "https://github.com/p3l6/XLineTool"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :sequoia"
 
   app "XLineTool.app"
 end
